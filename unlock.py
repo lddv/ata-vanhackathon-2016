a611c3091860e6d6e066f135c0c62fe74b1b64d4
lddv <leodias.vasconcelos@yahoo.com.br>
2022-12-20 22:02:55 -0600

    Extract function to remove every second person from circle

--- unlock.py
+++ unlock.py
@@ -6,35 +6,33 @@ def create_circle(number_of_people):
 def has_even_length(a_list):
     return len(a_list) % 2 == 0
 
+def sieve_pass(a_list):
+    return list( filter(lambda x: (a_list.index(x) % 2 == 0), a_list) )
+
 # when removed_last is True, we must start removing from the first position
 
 def process_circle(a_circle):
     my_circle = list(a_circle)
-    current_length = len(my_circle)
     removed_last = None
-    while current_length > 1:
+    while len(my_circle) > 1:
         if has_even_length(my_circle):
             if removed_last == False:
-                my_circle = filter(lambda x: my_circle.index(x) % 2 == 1, my_circle)
-                current_length = len(my_circle)
+                my_circle = sieve_pass(my_circle[1:])
             else:
-                my_circle = filter(lambda x: my_circle.index(x) % 2 == 0, my_circle)
-                current_length = len(my_circle)
+                my_circle = sieve_pass(my_circle)
                 removed_last = True
         else:
             # odd length
             if removed_last == False:
                 last_number = my_circle[-1]
-                my_circle = filter(lambda x: my_circle.index(x) % 2 == 1, my_circle)
-                current_length = len(my_circle)
+                my_circle = sieve_pass(my_circle[1:])
                 if last_number not in my_circle:
                     removed_last = True
             else:
-                my_circle = filter(lambda x: my_circle.index(x) % 2 == 0, my_circle)
-                current_length = len(my_circle)
+                my_circle = sieve_pass(my_circle)
                 removed_last = False
 
-        print(my_circle, current_length)
+        print(my_circle, len(my_circle))
     
     return my_circle[0]
 
